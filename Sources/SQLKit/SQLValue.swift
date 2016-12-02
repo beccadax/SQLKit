dd6d979500bc5ea401bfd448c3022f85e07155c0
Brent Royal-Gordon <brent@architechies.com>
2016-12-01 22:05:56 -0800

    Remove SQLValue requirements

--- Sources/SQLKit/SQLValue.swift
+++ Sources/SQLKit/SQLValue.swift
@@ -11,22 +11,34 @@ import Foundation
 /// Conforming types can be taken as parameters or returned as values by at least 
 /// one SQL client.
 /// 
-/// A `SQLValue` can be converted to and from a `sqlLiteral` representation. This is 
-/// a string without quoting or escaping which could be used to set the column 
-/// in your database.
+/// `SQLValue` does not have any requirements, and it cannot guarantee that the 
+/// specific SQL client you're using will be able to handle the `SQLValue` you 
+/// provide. Rather, it serves to weed out types which are not supported by *any* 
+/// SQL client. 
 /// 
-/// Individual SQL clients may ignore the `sqlLiteral` representation and substitute 
-/// their own.
+/// SQL clients are required to support the following types:
+/// 
+/// * `String`
+/// * `Data`
+/// * `Bool`
+/// * `Int`
+/// * `Double`
+/// * `Date`
+/// * Types conforming to `SQLStringConvertible` (treated as `String`s)
+/// 
+/// When a SQL client encounters a type it doesn't support, it should throw 
+/// `SQLValueError.typeUnsupportedByClient`. They should also conform any 
+/// additional supported types to `SQLValue`.
 /// 
 /// - SeeAlso: `SQLStringConvertible`
-public protocol SQLValue {
-    /// Creates a value of this type from the SQL literal string. The string has no 
-    /// quotes or escaping.
-    init?(sqlLiteral string: String)
-    /// A string which a typical SQL engine would be happy to accept in order to set 
-    /// or match this value. The string should have no quotes or escaping.
-    var sqlLiteral: String { get }
-}
+public protocol SQLValue {}
+
+extension String: SQLValue {}
+extension Data: SQLValue {}
+extension Bool: SQLValue {}
+extension Int: SQLValue {}
+extension Double: SQLValue {}
+extension Date: SQLValue {}
 
 /// Conforming types should be represented to SQL clients as the string returned by 
 /// the `sqlLiteral` property.
@@ -35,11 +47,29 @@ public protocol SQLValue {
 /// correspond to any type the SQL server is expected to support; rather, it should 
 /// be stored in something like a VARCHAR or TEXT column. For instance, `URL` 
 /// is a `SQLStringConvertible` type.
-public protocol SQLStringConvertible: SQLValue {}
+/// 
+/// A `LosslessStringConvertible` type which is conformed to `SQLStringConvertible` 
+/// will automatically have that conformance used by `SQLStringConvertible`.
+public protocol SQLStringConvertible: SQLValue {
+    /// Creates a value of this type from the SQL literal string. The string has no 
+    /// quotes or escaping.
+    /// 
+    /// - Throws: If the value cannot be converted. Use 
+    ///             `SQLValueError.stringNotConvertible` if there is no more specific 
+    ///             information available.
+    init(sqlLiteral string: String) throws
+    
+    /// A string which a typical SQL engine would be happy to accept in order to set 
+    /// or match this value. The string should have no quotes or escaping.
+    var sqlLiteral: String { get }
+}
 
-extension SQLValue where Self: LosslessStringConvertible {
-    public init?(sqlLiteral string: String) {
-        self.init(string)
+extension SQLStringConvertible where Self: LosslessStringConvertible {
+    public init(sqlLiteral string: String) throws {
+        guard let value = Self(string) else {
+            throw SQLValueError.stringNotConvertible(sqlLiteral: string, type: Self.self)
+        }
+        self = value
     }
     
     public var sqlLiteral: String {
@@ -47,54 +77,13 @@ extension SQLValue where Self: LosslessStringConvertible {
     }
 }
 
-extension String: SQLValue {}
-extension Int: SQLValue {}
-
-extension Date: SQLValue {
-    private static let postgresFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSSSSS"
-        return formatter
-    }()
-    
-    /// Converts from a date represented in a typical ISO-8601-style SQL format.
-    /// 
-    /// By default, `Date`s are represented in UTC in the format 
-    /// `yyyy-MM-dd HH:mm:ss.SSSSSS`.
-    public init?(sqlLiteral string: String) {
-        guard let d = Date.postgresFormatter.date(from: string) else {
-            return nil
-        }
-        self = d
-    }
-    
-    /// Converts to a date represented in a typical ISO-8601-style SQL format.
-    /// 
-    /// By default, `Date`s are represented in UTC in the format 
-    /// `yyyy-MM-dd HH:mm:ss.SSSSSS`.
-    public var sqlLiteral: String {
-        return Date.postgresFormatter.string(from: self)
-    }
-}
-
-extension Data: SQLValue {
-    /// Converts from a blob literal represented in hexadecimal format.
-    public init?(sqlLiteral string: String) {
-        self.init(hexEncoded: string)
-    }
-    
-    /// Converts to a blob literal represented in hexadecimal format.
-    public var sqlLiteral: String {
-        return hexEncodedString()
-    }
-}
-
 extension URL: SQLStringConvertible {
     /// Converts from a URL represented as a string.
-    public init?(sqlLiteral string: String) {
-        self.init(string: string)
+    public init(sqlLiteral string: String) throws {
+        guard let value = URL(string: string) else {
+            throw SQLValueError.stringNotConvertible(sqlLiteral: string, type: type(of: self))
+        }
+        self = value
     }
     
     /// Converts to a URL represented as a string.
