75b39c9a9421ff114b9a854b45680d55064c1d69
Brent Royal-Gordon <brent@architechies.com>
2016-12-03 05:37:06 -0800

    Provide localization for PG error messages

--- Sources/CorePostgreSQL/Error.swift
+++ Sources/CorePostgreSQL/Error.swift
@@ -14,13 +14,43 @@ public protocol PGConversionParsingState {
 
 public enum PGConversionError: Error {
     case invalidNumber(String)
-    
     case invalidDate(Error, at: String.Index, in: String, during: PGConversionParsingState)
+    case invalidInterval(Error, at: String.Index, in: String, during: PGConversionParsingState)
+    
     case unexpectedCharacter(Character)
     case invalidTimeZoneOffset(Int)
     case earlyTermination
     
-    case invalidInterval(Error, at: String.Index, in: String, during: PGConversionParsingState)
     case redundantQuantity(oldValue: Int, newValue: Int, for: PGInterval.Component)
     case unitlessQuantity(Int)
+}
+
+extension PGConversionError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .invalidNumber(let digits):
+            return NSLocalizedString("Could not parse '\(digits)' into a number.", comment: "")
+            
+        case let .invalidDate(error, at: _, in: _, during: state):
+            return NSLocalizedString("Error \(state.localizedStateDescription) in a date: \(error.localizedDescription)", comment: "")
+        
+        case let .invalidInterval(error, at: _, in: _, during: state):
+            return NSLocalizedString("Error \(state.localizedStateDescription) in an interval: \(error.localizedDescription)", comment: "")
+            
+        case .unexpectedCharacter(let char):
+            return NSLocalizedString("Unexpectedly encountered a '\(char)'.", comment: "")
+            
+        case .invalidTimeZoneOffset(let offset):
+            return NSLocalizedString("'\(offset)' is not a valid time zone.", comment: "")
+            
+        case .earlyTermination:
+            return NSLocalizedString("Unexpectedly reached the end of the string.", comment: "")
+            
+        case let .redundantQuantity(oldValue, newValue, for: component):
+            return NSLocalizedString("It specified both \(oldValue) and \(newValue) for the \(component).", comment: "")
+            
+        case .unitlessQuantity(let number):
+            return NSLocalizedString("No unit was specified for the number \(number).", comment: "")
+        }
+    }
 }
