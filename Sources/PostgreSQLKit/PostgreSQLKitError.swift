cd8750294b8cc97d47bd8006af08d989f59480bb
Brent Royal-Gordon <brent@architechies.com>
2016-12-04 22:15:07 -0800

    Make `PostgreSQLKitError` public and documented

--- Sources/PostgreSQLKit/PostgreSQLKitError.swift
+++ Sources/PostgreSQLKit/PostgreSQLKitError.swift
@@ -9,12 +9,20 @@
 import Foundation
 import CorePostgreSQL
 
-enum PostgreSQLKitError: Error {
+/// Errors specific to `PostgreSQLKit`.
+/// 
+/// Note that most errors you might find underlying `SQLError`s will *not* be 
+/// `PostgreSQLKitError`s, but rather `PGError`s from the underlying 
+/// `CorePostgreSQL` library. If you want to perform rich, detailed matching of 
+/// errors, use that type to do so.
+public enum PostgreSQLKitError: Error {
+    /// The timestamp does not correspond to a valid date in the Gregorian 
+    /// calendar, so it cannot be converted to a `Date`.
     case invalidDate(PGTimestamp)
 }
 
 extension PostgreSQLKitError: LocalizedError {
-    var errorDescription: String? {
+    public var errorDescription: String? {
         switch self {
         case .invalidDate(let timestamp):
             return NSLocalizedString("Cannot convert \(timestamp) to an NSDate.", comment: "")
