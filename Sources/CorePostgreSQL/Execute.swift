27b24d6f494fd88eae42c7fe0cc309e0a9d7a698
Brent Royal-Gordon <brent@architechies.com>
2016-12-04 23:51:07 -0800

    Document the various execute(…) methods

--- Sources/CorePostgreSQL/Execute.swift
+++ Sources/CorePostgreSQL/Execute.swift
@@ -10,12 +10,43 @@ import Foundation
 import libpq
 
 extension PGConn {
+    /// Execute one or more SQL statements, returning the result of the last 
+    /// statement. 
+    /// 
+    /// - Parameter sql: The SQL to run.
+    /// - Returns: The result of the last statement in the SQL to run.
+    /// - Throws: `SQLError.executionFailed` if any of the statements failed.
+    /// 
+    /// - Note: If you need to substitute any values into the statement, use 
+    ///          `execute(_:with:resultingIn:)` instead. That method will pass the 
+    ///          parameters out-of-band, avoiding the risk of a SQL injection attack.
+    /// 
     /// - RecommendedOver: `PQexec`
     public func execute(_ sql: String) throws -> PGResult {
         let resultPointer = PQexec(pointer, sql)!
         return try PGResult(pointer: resultPointer)
     }
     
+    /// Executes a SQL statement, binding the indicated parameter raw values to any 
+    /// placeholders with the indicated types.
+    /// 
+    /// - Parameter sql: The SQL to run, with placeholders of the form `$1`, `$2`, 
+    ///               etc. marking the parameters.
+    /// - Parameter parameterValues: The parameters expressed as `PGRawValue`s. 
+    ///               `nil` values will be treated as `NULL`.
+    /// - Parameter parameterTypes: The types of the parameters. `nil` or 
+    ///               unspecified parameters will have a type automatically chosen.
+    /// - Parameter resultFormat: The format the columns in the result should be 
+    ///               returned in. This should be `textual` unless all of the types 
+    ///               of the columns conform to `SQLBinaryValue`.
+    /// - Returns: The result of the statement.
+    /// - Throws: `SQLError.executionFailed` if the statement failed.
+    /// 
+    /// - Note: This call requires you to manually create `PGRawValue`s for the 
+    ///          parameters. It will usually be more convenient to use 
+    ///          `execute(_:with:resultingIn:)`, which automatically converts its 
+    ///          `PGValue` parameters into `PGRawValue`s.
+    /// 
     /// - RecommendedOver: `PQexecParams`
     public func execute(_ sql: String, withRaw parameterValues: [PGRawValue?], ofTypes parameterTypes: [PGType?] = [], resultingIn resultFormat: PGRawValue.Format = .textual) throws -> PGResult {
         var typeOids = oids(of: parameterTypes)
@@ -33,11 +64,37 @@ extension PGConn {
         return try PGResult(pointer: resultPointer)
     }
     
+    /// Executes a SQL statement, binding the indicated parameter values to any 
+    /// placeholders.
+    /// 
+    /// - Parameter sql: The SQL to run, with placeholders of the form `$1`, `$2`, 
+    ///               etc. marking the parameters.
+    /// - Parameter parameterValues: The parameters, which must be `PGValue`s. 
+    ///               `nil` values will be treated as `NULL`.
+    /// - Parameter resultFormat: The format the columns in the result should be 
+    ///               returned in. This should be `textual` unless all of the types 
+    ///               of the columns conform to `SQLBinaryValue`.
+    /// - Returns: The result of the statement.
+    /// - Throws: `SQLError.executionFailed` if the statement failed.
+    /// 
+    /// - RecommendedOver: `PQexecParams`, `execute(_:withRaw:ofTypes:resultingIn:)`
     public func execute(_ sql: String, with parameterValues: [PGValue?], resultingIn resultFormat: PGRawValue.Format = .textual) throws -> PGResult {
         let (rawParameters, types) = rawValues(of: parameterValues)
         return try execute(sql, withRaw: rawParameters, ofTypes: types, resultingIn: resultFormat)
     }
     
+    /// Prepares a SQL statement for later execution.
+    /// 
+    /// - Parameter sql: The SQL to run, with placeholders of the form `$1`, `$2`, 
+    ///               etc. marking the parameters.
+    /// - Parameter types: The types of the parameters. `nil` or unspecified  
+    ///               parameters will have a type automatically chosen.
+    /// - Parameter name: The name of the prepared statement. If `nil`, a name 
+    ///               will be generated automatically.
+    /// - Returns: A `PGPreparedStatement` instance which can be used to execute 
+    ///             the query.
+    /// - Throws: `SQLError.executionFailed` if preparing the statement failed.
+    /// 
     /// - RecommendedOver: `PQprepare`
     public func prepare(_ sql: String, withRawTypes types: [PGType?] = [], name: String? = nil) throws -> PGPreparedStatement {
         let name = name ?? ProcessInfo.processInfo.globallyUniqueString
@@ -51,6 +108,24 @@ extension PGConn {
 }
 
 extension PGPreparedStatement {
+    /// Executes a prepared SQL statement, binding the indicated parameter raw  
+    /// values to any placeholders.
+    /// 
+    /// - Parameter parameterValues: The parameters expressed as `PGRawValue`s. 
+    ///               `nil` values will be treated as `NULL`.
+    /// - Parameter resultFormat: The format the columns in the result should be 
+    ///               returned in. This should be `textual` unless all of the types 
+    ///               of the columns conform to `SQLBinaryValue`.
+    /// - Returns: The result of the statement.
+    /// - Throws: `SQLError.executionFailed` if the statement failed.
+    /// 
+    /// - Note: This call requires you to manually create `PGRawValue`s for the 
+    ///          parameters. It will usually be more convenient to use 
+    ///          `execute(with:resultingIn:)`, which automatically converts its 
+    ///          `PGValue` parameters into `PGRawValue`s.
+    /// 
+    /// - Precondition: The prepared statement has not been deallocated.
+    /// 
     /// - RecommendedOver: `PQexecPrepared`
     public func execute(withRaw parameterValues: [PGRawValue?], resultingIn resultFormat: PGRawValue.Format = .textual) throws -> PGResult {
         guard let name = self.name else {
@@ -64,7 +139,20 @@ extension PGPreparedStatement {
         return try PGResult(pointer: resultPointer)
     }
     
-    
+    /// Executes a prepared SQL statement, binding the indicated parameter   
+    /// values to any placeholders.
+    /// 
+    /// - Parameter parameterValues: The parameters, which must be `PGValue`s. 
+    ///               `nil` values will be treated as `NULL`.
+    /// - Parameter resultFormat: The format the columns in the result should be 
+    ///               returned in. This should be `textual` unless all of the types 
+    ///               of the columns conform to `SQLBinaryValue`.
+    /// - Returns: The result of the statement.
+    /// - Throws: `SQLError.executionFailed` if the statement failed.
+    /// 
+    /// - Precondition: The prepared statement has not been deallocated.
+    /// 
+    /// - RecommendedOver: `PQexecPrepared`
     public func execute(with parameterValues: [PGValue?], resultingIn resultFormat: PGRawValue.Format = .textual) throws -> PGResult {
         let (rawParameters, _) = rawValues(of: parameterValues)
         return try execute(withRaw: rawParameters, resultingIn: resultFormat)
